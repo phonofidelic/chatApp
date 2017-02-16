d79d4b91e4e6dc5ea6121e1442765347851b5f0f
phonofidelic <chris_clemons85@hotmail.com>
2017-02-16 03:36:38 -0800

    delete unused files

--- public/app/app.config.js
+++
@@ -1,9 +0,0 @@
-// angular.module(['chatApp'])
-// .config(['$routeProvider', '$locationProvider',function($routeProvider, $locationProvider) {
-// 	$routeProvider
-// 		.when('/', {
-// 			templateUrl: 'app/chat.html',
-// 			controller: 'chatController',
-// 			conrollerAs: 'vm'
-// 		});
-// }]);
