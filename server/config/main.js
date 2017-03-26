8551ce6b155cecd17d27f787f763c7e0326871af
phonofidelic <chris_clemons85@hotmail.com>
2017-03-25 19:16:49 -0700

    try new build. New yarn install of mailgun-js.

--- server/config/main.js
+++ server/config/main.js
@@ -1,6 +1,7 @@
 module.exports = {
 	'secret': 'secret', // temp?
-	'database': 'mongodb://localhost:27017/chatApp',
+	// 'database': 'mongodb://localhost:27017/chatApp',
+	'database': 'mongodb://heroku_djrv4shx:3q2trf5i3gbddvd8dtgpa0u2ca@ds139959.mlab.com:39959/heroku_djrv4shx',
 	'port': process.env.PORT || 3001,
 	// mailgun config:
 	'mailgun_priv_key': 'key-728d106a56681eab30e04ef06819bc4a',
