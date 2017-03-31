cd21b008810e31cd47431f5a087c9434df26242d
phonofidelic <chris_clemons85@hotmail.com>
2017-03-30 19:00:16 -0700

    new build
    Changed Access-Control-Allow-Origin to allow all origins.

--- server/server.js
+++ server/server.js
@@ -73,7 +73,7 @@ app.use(logger('dev')); // Log requests to API using morgan
 // });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
   res.header('Access-Control-Allow-Credentials', 'true');
