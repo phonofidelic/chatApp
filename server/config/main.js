ed46aa231c341dfc38420c1fa7f2bcf46897869f
phonofidelic <chris_clemons85@hotmail.com>
2017-04-08 17:54:56 -0700

    new build

--- server/config/main.js
+++ server/config/main.js
@@ -6,7 +6,5 @@ module.exports = {
 	'port': process.env.PORT || 3001,
 	'mailgun_priv_key': process.env.MAILGUN_PRIV_KEY || 'key-728d106a56681eab30e04ef06819bc4a',
 	'mailgun_domain': 'sandbox4fc771de5236465e9e5d91ae2fd03616.mailgun.org',
-	// 'port': 3000,
-	// 'test_port': 3001,
 	'test_env': 'test'
 }
