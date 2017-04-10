4cf49cb913d91e683406523a36bfd4131eb33738
phonofidelic <chris_clemons85@hotmail.com>
2017-04-10 16:43:06 -0700

    comment out app.get... in static_server/index.js

--- static_server/index.js
+++ static_server/index.js
@@ -9,9 +9,9 @@ var staticFiles = express.static(path.join(__dirname, '../client/build'));
 
 app.use(staticFiles);
 
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-})
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+// })
 
 var server= app.listen(process.env.PORT || 8080, function() {
 	console.log('Static files served on port 8080');
